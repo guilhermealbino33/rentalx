43514b7910bbea04c1a47dcb06a27492a36db92a
Guilherme Albino <guilhermealbino33@gmail.com>
2021-11-30 11:43:27 -0300

    create user add
    Having troubles with specifications table

---
+++ src/modules/accounts/repositories/implementatios/UsersRepository.ts
@@ -0,0 +1,33 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/user";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UserRepository implements IUsersRepository {
+    private repository: Repository<User>;
+
+    constructor() {
+        this.repository = getRepository(User);
+    }
+
+    async create({
+        name,
+        username,
+        email,
+        driver_license,
+        password,
+    }: ICreateUserDTO): Promise<void> {
+        const user = this.repository.create({
+            name,
+            username,
+            email,
+            driver_license,
+            password,
+        });
+
+        await this.repository.save(user);
+    }
+}
+
+export { UserRepository };
