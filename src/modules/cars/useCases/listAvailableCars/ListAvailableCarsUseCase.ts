af53d0e81554acce50c4f0d46407696a43652462
Guilherme Albino <guilhermealbino33@gmail.com>
2021-12-27 16:55:50 -0300

    resolving import export issue

--- src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -1,3 +1,5 @@
+import { inject, injectable } from "tsyringe";
+
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 
@@ -6,11 +8,14 @@ interface IRequest {
     brand?: string;
     name?: string;
 }
+@injectable()
+class ListAvailableCarsUseCase {
+    constructor(
+        @inject("CarsRepository")
+        private carsRepository: ICarsRepository
+    ) {}
 
-class ListCarsUseCase {
-    constructor(private carsRepository: ICarsRepository) {}
-
-    async execute({ brand, category_id, name }: IRequest): Promise<Car[]> {
+    async execute({ category_id, brand, name }: IRequest): Promise<Car[]> {
         const cars = await this.carsRepository.findAvailable(
             brand,
             category_id,
@@ -20,4 +25,4 @@ class ListCarsUseCase {
     }
 }
 
-export { ListCarsUseCase };
+export { ListAvailableCarsUseCase };
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,18 +1,21 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
-
+const listAvailablecarsController = new ListAvailableCarsController();
 carsRoutes.post(
     "/",
     ensureAuthenticated,
     ensureAdmin,
     createCarController.handle
 );
+
+carsRoutes.get("/available", listAvailablecarsController.handle);
 
 export { carsRoutes };
