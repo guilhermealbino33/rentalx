125bc71ebc18b389db02a0f2998a1e6979a98cf4
Guilherme Albino <guilhermealbino33@gmail.com>
2022-02-10 11:24:17 -0300

    seed fixed

--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -4,26 +4,18 @@ import { v4 as uuidV4 } from "uuid";
 import createConnection from "../index";
 
 async function create() {
-    const connection = await createConnection("localhost");
-    const id = uuidV4;
+    const connection = await createConnection();
+
+    const id = uuidV4();
     const password = await hash("admin", 8);
 
     await connection.query(
-        `insert into users (
-            id, 
-            name, 
-            email, 
-            password, 
-            'isAdmin', 
-            created_at,
-            driver_license) 
-        values(${id},
-             'admin',
-             'admin@rentx.com',
-              ${password},
-              true,
-              'now()',
-              'XXXXXX')`
+        `INSERT INTO USERS(id, name, email, password, "is_admin", created_at, driver_license ) 
+      values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
+    `
     );
+
+    await connection.close();
 }
-create().then(() => console.log("User admin created"));
+
+create().then(() => console.log("User admin created!"));
