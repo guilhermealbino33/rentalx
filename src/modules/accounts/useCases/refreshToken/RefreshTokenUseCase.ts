3432fd75ce409ee7383fb4511e83db1f80b9ef94
Guilherme Albino <guilhermealbino33@gmail.com>
2022-02-17 10:39:24 -0300

    refresh token

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -1,4 +1,4 @@
-import { sign, verify } from "jsonwebtoken";
+import { verify, sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
 import auth from "@config/auth";
@@ -10,8 +10,13 @@ interface IPayload {
     sub: string;
     email: string;
 }
-injectable();
 
+interface ITokenResponse {
+    token: string;
+    refresh_token: string;
+}
+
+@injectable()
 class RefreshTokenUseCase {
     constructor(
         @inject("UsersTokensRepository")
@@ -19,14 +24,15 @@ class RefreshTokenUseCase {
         @inject("DayJsDateProvider")
         private dateProvider: IDateProvider
     ) {}
-    async execute(token: string): Promise<string> {
-        const {
-            secret_refresh_token,
-            expires_in_refresh_token,
-            expires_refresh_token_days,
-        } = auth;
-        const { email, sub } = verify(token, secret_refresh_token) as IPayload;
+
+    async execute(token: string): Promise<ITokenResponse> {
+        const { email, sub } = verify(
+            token,
+            auth.secret_refresh_token
+        ) as IPayload;
+
         const user_id = sub;
+
         const userToken =
             await this.usersTokensRepository.findByUserIdAndRefreshToken(
                 user_id,
@@ -34,24 +40,42 @@ class RefreshTokenUseCase {
             );
 
         if (!userToken) {
-            throw new AppError("Refresh Token does note exists!");
+            throw new AppError("Refresh Token does not exists!");
         }
+
         await this.usersTokensRepository.deleteById(userToken.id);
+        const {
+            secret_refresh_token,
+            expires_in_refresh_token,
+            expires_refresh_token_days,
+            secret_token,
+            expires_in_token,
+        } = auth;
 
         const refresh_token = sign({ email }, secret_refresh_token, {
             subject: sub,
             expiresIn: expires_in_refresh_token,
         });
+
         const expires_date = this.dateProvider.addDays(
             expires_refresh_token_days
         );
+
         await this.usersTokensRepository.create({
             expires_date,
             refresh_token,
             user_id,
         });
 
-        return refresh_token;
+        const newToken = sign({}, secret_token, {
+            subject: user_id,
+            expiresIn: expires_in_token,
+        });
+
+        return {
+            refresh_token,
+            token: newToken,
+        };
     }
 }
 
--- src/modules/cars/infra/typeorm/repositories/CategoriesRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CategoriesRepository.ts
@@ -24,7 +24,7 @@ class CategoriesRepository implements ICategoriesRepository {
     }
 
     async list(): Promise<Category[]> {
-        const categories = this.repository.find();
+        const categories = await this.repository.find();
         return categories;
     }
 
