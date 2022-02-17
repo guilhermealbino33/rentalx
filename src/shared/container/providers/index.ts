26a792396c3bbcaddd079b9dcad1637e2e5344aa
Guilherme Albino <guilhermealbino33@gmail.com>
2022-02-17 11:36:12 -0300

    mail provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,14 @@ import { container } from "tsyringe";
 
 import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayJsDateProvider } from "./DateProvider/implementations/DayJsDateProvider";
+import { IMailProvider } from "./MailProvider/IMailProvider";
+import { EtherealMailProvider } from "./MailProvider/Implementations/EtherealMailProvider";
 
 container.registerSingleton<IDateProvider>(
     "DayJsDateProvider",
     DayJsDateProvider
+);
+container.registerSingleton<IMailProvider>(
+    "EtherealMailProvider",
+    EtherealMailProvider
 );
